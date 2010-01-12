a4419993eeba79e45fadf0a13d57cfd01e3e0de0
Bert Burgemeister <trebbu@googlemail.com>
2010-01-12 12:05:04 +0100

    More tests, still unfinished.

--- test/test.sh
+++ test/test.sh
@@ -1,11 +1,15 @@
 #! /bin/bash
 
+# Caveats: kills all killable rsyncs
+#          don't disturb test timing by putting too much (extra) load on the machine
 # Debian: install packages bc, time
 
 TESTDIR=/tmp/monikop-test
 DEV=$TESTDIR/dev
 MNT=$TESTDIR/mnt
 LOG=$TESTDIR/log
+MONIKOP_1="../monikop ../test/monikop.config.test.1"
+MONIKOP_2="../monikop ../test/monikop.config.test.2"
 TEST_COUNT=0
 FAIL_COUNT=0
 FAILED_TESTS=""
@@ -15,6 +19,14 @@ function make_test_drive {
     mkdir -p $MNT/$1
     dd if=/dev/zero of=$DEV/$1 bs=1024 count=$2 2> /dev/null
     /sbin/mkfs.ext3 -m 0 -Fq $DEV/$1
+    if ! mount $MNT/$1 2> /dev/null; then
+        echo "# Can't mount $DEV/$1 to $MNT/$1."
+        echo "# Redo from start after adding the following line to your /etc/fstab:"
+        echo 
+        echo "   $DEV/$1 $MNT/$1 ext3 loop,user,noauto 0 0"
+        echo
+        return 1
+    fi
 }
 
 # make_test_file <name> <size> <date>
@@ -36,7 +48,7 @@ function find_and_compare {
     diverging_mtime=""
     return_value=0
     for i in `find $origin_dirs -type f 2> /dev/null`; do
-        found=`find $@ -path "$MNT/*/measuring_data/${i#$MNT/*/data/}"`
+        found=`find $@ -path "$MNT/*/measuring_data/${i#$MNT/*/data/}" 2> /dev/null`
         if [[ $found == "" ]] ; then
             missing="$missing $i";
         elif ! cmp --quiet $i $found; then
@@ -60,36 +72,37 @@ function find_and_compare {
     return $return_value
 }
 
-# run_test <expected_return> <command>
+# run_test <expected_return> <test-command>
 function run_test {
+    echo "RUNNING $2"
     TEST_COUNT=$(( TEST_COUNT + 1 ))
     $2
     RETURN_VALUE=$?
     if [[ $RETURN_VALUE -ne $1 ]]; then
         FAIL_COUNT=$(( FAIL_COUNT + 1 ))
-        FAILED_TESTS="$FAILED_TESTS$2($1? $RETURN_VALUE!)\n"
+        FAILED_TESTS="\n$FAILED_TESTS$2($1? $RETURN_VALUE!)"
         echo "$2 should have returned $1 but returned $RETURN_VALUE instead."
     fi
 }
 
-umount $MNT/* 2> /dev/null
+umount $MNT/* #2> /dev/null
 rm -rf $DEV $MNT $LOG
 mkdir -p $DEV $MNT
 
 # Create and mount test drives:
 for i in 01 02 03 04; do
     make_test_drive $i 102400
-    if ! mount $MNT/$i 2> /dev/null; then
-        echo "# Can't mount $DEV/$i to $MNT/$i."
-        echo "# Redo from start after adding the following line to your /etc/fstab:"
-        echo 
-        echo "   $DEV/$i $MNT/$i ext3 loop,user,noauto 0 0"
-        echo
+    if [[ $? == 1 ]]; then
         MOUNTING_PROBLEM=1
     fi
 done
+make_test_drive 05 102400
+if [[ $? == 1 ]]; then
+    MOUNTING_PROBLEM=1
+fi
 if [[ $MOUNTING_PROBLEM == 1 ]]; then exit; fi
 
+exit
 
 # Prepare data sources:
 mkdir -p $MNT/01/data/d1/d2
@@ -114,43 +127,126 @@ done
 
 T1=`/usr/bin/time --format="%e" rsync --recursive --times $MNT/01/data/ $MNT/03/ 2>&1 &`
 T2=`/usr/bin/time --format="%e" rsync --recursive --times $MNT/02/data/ $MNT/04/ 2>&1 &`
-INTERRUPTION_TIME_0=`echo "($T1 + $T2) * 1.5" | bc`
+INTERRUPTION_TIME_0=`echo "($T1 + $T2) * 3" | bc`
 INTERRUPTION_TIME_1=`echo "($T1 + $T2) * .16" | bc`
 INTERRUPTION_TIME_2=`echo "($T1 + $T2) * .82" | bc`
-echo $INTERRUPTION_TIME_1
+echo $INTERRUPTION_TIME_0
 rm -rf $MNT/03/* $MNT/04/*
 
-# Stuff one of the destinations a bit:
-# make_test_file $MNT/03/stuffing 25000 199903250845
 
 function test_monikop_simple {
-    ../monikop ../test/monikop.config.test & sleep $INTERRUPTION_TIME_1; /bin/kill -TERM $!
-    find_and_compare /tmp/monikop-test/mnt/01/data /tmp/monikop-test/mnt/02/data :: /tmp/monikop-test/mnt/04/measuring_data /tmp/monikop-test/mnt/03/measuring_data
+    $MONIKOP_1 & sleep $INTERRUPTION_TIME_0; /bin/kill -TERM $!
+    find_and_compare $TESTDIR/mnt/0{1,2}/data :: $TESTDIR/mnt/0{3,4}/measuring_data
 }
 
-run_test 0 test_monikop_simple
-run_test 0 test_monikop_simple
+function test_monikop_short {
+    $MONIKOP_1 & sleep $INTERRUPTION_TIME_1; /bin/kill -TERM $!
+    find_and_compare $TESTDIR/mnt/0{1,2}/data :: $TESTDIR/mnt/0{3,4}/measuring_data
+}
 
-echo $TEST_COUNT
-echo $FAIL_COUNT
-echo -e $FAILED_TESTS
+function test_monikop_short_kill_rsync_first {
+    $MONIKOP_2 & sleep $INTERRUPTION_TIME_1; /usr/bin/killall -KILL rsync; sleep 1; /bin/kill -TERM $!
+    find_and_compare $TESTDIR/mnt/0{1,2}/data :: $TESTDIR/mnt/0{3,4,5}/measuring_data
+}
 
-exit
-../monikop ../test/monikop.config.test & sleep $INTERRUPTION_TIME_2; /bin/kill -TERM $!
-sleep 2
-find_and_compare /tmp/monikop-test/mnt/01/data /tmp/monikop-test/mnt/02/data :: /tmp/monikop-test/mnt/04/measuring_data /tmp/monikop-test/mnt/03/measuring_data
-if [[ $? -eq 0 ]]; then
-    echo "Shouldn't be finished at this stage!"
-else
-    echo "This is nothing to worry about."
-fi
-../monikop ../test/monikop.config.test & sleep $INTERRUPTION_TIME_2; /bin/kill -TERM $!
-sleep 2
-../monikop ../test/monikop.config.test & sleep $INTERRUPTION_TIME_2; /bin/kill -TERM $!
-find_and_compare /tmp/monikop-test/mnt/01/data /tmp/monikop-test/mnt/02/data :: /tmp/monikop-test/mnt/04/measuring_data /tmp/monikop-test/mnt/03/measuring_data
-echo $?
-#../monikop monikop.config.test & sleep $INTERRUPTION_TIME_1; kill $!
-# ../monikop ../test/pokinom.config.test & sleep $INTERRUPTION_TIME_1; killall rsync (make sure we kill only our rsyncs)
-#
-#
-#
+function test_monikop_short_umount_sources {
+    $MONIKOP_2 & sleep $INTERRUPTION_TIME_1; umount -l $TESTDIR/mnt/0{1,2}; sleep 1; /bin/kill -TERM $!
+    find_and_compare $TESTDIR/mnt/0{1,2}/data :: $TESTDIR/mnt/0{3,4,5}/measuring_data
+    mount $TESTDIR/mnt/01
+    mount $TESTDIR/mnt/02
+}
+
+function test_monikop_short_umount_destinations {
+    $MONIKOP_2 & sleep $INTERRUPTION_TIME_1; umount -l $TESTDIR/mnt/0{3,4,5}; sleep 1; /bin/kill -TERM $!
+    find_and_compare $TESTDIR/mnt/0{1,2}/data :: $TESTDIR/mnt/0{3,4,5}/measuring_data
+    mount $TESTDIR/mnt/03
+    mount $TESTDIR/mnt/04
+    mount $TESTDIR/mnt/05
+}
+
+function test_monikop_simple_2 {
+    $MONIKOP_2 & sleep $INTERRUPTION_TIME_0; /bin/kill -TERM $!
+    find_and_compare $TESTDIR/mnt/0{1,2}/data :: $TESTDIR/mnt/0{3,4,5}/measuring_data
+}
+
+function test_monikop_overflow {
+# Stuff one of the destinations a bit:
+    make_test_file $MNT/03/stuffing 25000 199903250845
+    $MONIKOP_1 & sleep $INTERRUPTION_TIME_0; /bin/kill -TERM $!
+    find_and_compare $TESTDIR/mnt/0{1,2}/data :: $TESTDIR/mnt/0{3,4}/measuring_data
+}
+
+function test_monikop_no_destination {
+# We test basically if there is something to kill.
+    umount $MNT/{03,04}
+    $MONIKOP_1 & sleep $INTERRUPTION_TIME_2; /bin/kill -TERM $!
+    RETURN=$?
+    mount $MNT/03
+    mount $MNT/04
+    return $RETURN
+}
+
+function test_monikop_no_source {
+# We test basically if there is something to kill.
+    umount $MNT/{01,02}
+    $MONIKOP_1 & sleep $INTERRUPTION_TIME_2; /bin/kill -TERM $!
+    RETURN=$?
+    mount $MNT/01
+    mount $MNT/02
+    return $RETURN
+}
+
+# run_test 0 test_monikop_simple
+# 
+# mv $MNT/03/measuring_data $MNT/03/backed_up
+# mv $MNT/04/measuring_data $MNT/04/backed_up
+# rm -rf $LOG
+# 
+# run_test 0 test_monikop_simple
+# 
+# rm -rf $MNT/0{3,4}/* $LOG
+# 
+# run_test 1 test_monikop_short
+# run_test 1 test_monikop_short
+# run_test 0 test_monikop_simple
+# 
+# mv $MNT/03/measuring_data $MNT/03/backed_up
+# mv $MNT/04/measuring_data $MNT/04/backed_up
+# rm -rf $LOG
+# 
+# run_test 1 test_monikop_short
+# run_test 1 test_monikop_short
+# run_test 0 test_monikop_simple
+# 
+# rm -rf $MNT/0{3,4}/* $LOG
+# 
+# run_test 1 test_monikop_overflow
+# 
+# rm -rf $MNT/0{3,4}/* $LOG
+# 
+# run_test 0 test_monikop_no_destination
+# run_test 0 test_monikop_no_source
+# 
+# rm -rf $MNT/0{3,4}/* $LOG
+# 
+# run_test 1 test_monikop_short_kill_rsync_first
+# run_test 0 test_monikop_simple_2
+# 
+# rm -rf $MNT/0{3,4,5}/* $LOG
+# 
+# run_test 1 test_monikop_short_umount_sources
+# run_test 0 test_monikop_simple_2
+
+rm -rf $MNT/0{3,4,5}/* $LOG
+
+run_test 1 test_monikop_short_umount_destinations
+run_test 0 test_monikop_simple_2
+
+rm -rf $MNT/0{3,4,5}/* $LOG
+
+
+echo "Total number of tests: $TEST_COUNT"
+echo "Number of failed tests: $FAIL_COUNT"
+echo -e "$FAILED_TESTS"
+
+exit $FAIL_COUNT
